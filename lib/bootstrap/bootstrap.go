2f95364895ffc8777f16d3d7098e0283c2e2e3a2
Sebastian Kreutzberger <s.kreutzberger@googlemail.com>
2021-09-23 12:56:07 +0200

    initial commit
    Signed-off-by: Sebastian Kreutzberger <s.kreutzberger@googlemail.com>

---
+++ lib/bootstrap/bootstrap.go
@@ -0,0 +1,53 @@
+package bootstrap
+
+import (
+	"os"
+
+	"github.com/iconmobile-dev/go-core/logger"
+	"github.com/iconmobile-dev/go-interview/config"
+)
+
+// LoggerAndConfig is returning logger & config which are
+// required for bootstrapping a service server
+// is using CONFIG_FILE env var and if not set uses
+// cfgFilePath. If cfgFilePath is not set then it tries to find the config
+func LoggerAndConfig(serverName string, test bool) (logger.Logger, config.Config) {
+	// init logger
+	log := logger.Logger{MinLevel: "verbose"}
+
+	var cfg *config.Config
+	var err error
+	// load config
+	configFile := os.Getenv("CONFIG_FILE")
+	if configFile == "" {
+		cfg, err = config.LoadDefaultConfig()
+		if err != nil {
+			log.Error(err, "config load")
+			os.Exit(1)
+		}
+	} else {
+		cfg, err = config.Load(configFile)
+		if err != nil {
+			log.Error(err, "config load")
+			os.Exit(1)
+		}
+	}
+
+	// set service name
+	log.MinLevel = cfg.Logging.MinLevel
+	log.TimeFormat = cfg.Logging.TimeFormat
+	log.UseColor = cfg.Logging.UseColor
+	log.ReportCaller = cfg.Logging.ReportCaller
+	cfg.Server.Name = serverName
+
+	if test {
+		log.UseColor = false
+		cfg.Server.Name += "_test"
+	}
+
+	if cfg.Server.Env == "prod" {
+		log.UseJSON = true
+	}
+
+	return log, *cfg
+}
